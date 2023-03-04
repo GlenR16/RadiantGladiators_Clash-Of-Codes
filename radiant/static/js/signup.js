a601c14855798d1639aff0f0f2ffc38d95246c22
Glen Rodrigues <glenrogers1234@gmail.com>
2023-03-04 21:06:08 +0530

    Verfication.

---
+++ radiant/static/js/signup.js
@@ -0,0 +1,20 @@
+function page2(){
+    document.getElementById("page_1").classList.add("hidden");
+    document.getElementById("page_2").classList.remove("hidden");
+  }
+  function page3(){
+    document.getElementById("page_2").classList.add("hidden");
+    document.getElementById("page_3").classList.remove("hidden");
+  }
+  function get_otp(){
+    document.querySelector("input[name='csrfmiddlewaretoken']");
+    let file = document.getElementById("file-upload");
+    let otp = document.getElementById("otp");
+    let password1 = document.getElementById("password");
+    let password2 = document.getElementById("c_password");
+    let otp = document.getElementById("otp");
+    let otp = document.getElementById("otp");
+    let otp = document.getElementById("otp");
+    let otp = document.getElementById("otp");
+    let otp = document.getElementById("otp");
+  }
