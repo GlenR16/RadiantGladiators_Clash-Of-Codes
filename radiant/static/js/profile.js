3fe36bc3282b035ba4457ef9776fbe307d33b0de
Glenn Mendonca <glenem17@gmail.com>
2023-03-05 03:24:58 +0530

    db commit

--- radiant/static/js/profile.js
+++ radiant/static/js/profile.js
@@ -7,7 +7,7 @@ interestsInput.addEventListener("change", addChip);
 function addChip(e){
     const name = e.target.value;
     let chipTemplate = `
-        <div class="gap-x-3 mt-3 bg-blue-400 inline p-2 rounded-3xl text-white">
+        <div class="gap-x-3 mt-3 bg-neutral-700 inline p-2 rounded-3xl text-white">
             <input type="checkbox" hidden name="${name}" value="true" />
             <div class="inline">${name}</div>
             <span onclick="this.parentNode.remove()" >x</span> 
