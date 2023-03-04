63ac60e68db17b4fb23e951f1728a36268e4c558
Glen Rodrigues <glenrogers1234@gmail.com>
2023-03-05 00:48:32 +0530

    Requirements.

--- home/views.py
+++ home/views.py
@@ -34,9 +34,11 @@ class ProfileView(TemplateView):
     template_name = "profile.html"
     
     def post(self, request, *args, **kwargs):
-        name = request.POST.get("name","")
-        phone = request.POST.get("phone","")
-        email = request.POST.get("email","")
+        bio = request.POST.get("bio","")
+        address = request.POST.get("address","")
+        profile_image = request.POST.get("profile_image","")
+        profile_image = request.POST.get("profile_image","")
+        profile_image = request.POST.get("profile_image","")
         
         return self.render_to_response({"form":form})
         
